d9227a4390290d9548dd666641a23d5154eea2b0
sufiyana <sufiyanakpa@gmil.com>
2024-08-28 09:52:35 +0400

    new

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class products(models.Model):
+    name=models.CharField(unique=True,max_length=100)
+    price=models.PositiveIntegerField()
+    description=models.CharField(max_length=300)
+    category=models.CharField(max_length=200)
+    image=models.ImageField(null=True)
+
+    def __str__(self):
+        return self.name
